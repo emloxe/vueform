c0507b78d58f48401311537f8b1b1d8602b8d79b
ZWJ <qwrtyp@live.cn>
2023-06-29 17:33:50 +0800

    添加docs

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,16 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: HomeView,
+  // },
   {
     path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/tool",
     name: "tool",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
